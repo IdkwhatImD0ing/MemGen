bcc4c59e2cf7ebd90bf58b3266656bdd8c2d7da7
Trique Nguyen <ngngoctridung123@gmail.com>
2023-04-22 01:45:22 -0700

    working on homepage

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,12 +1,13 @@
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { Inter, Montserrat } from 'next/font/google'
 import Navbar from './components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
+const montserrat = Montserrat({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className='bg-black min-h-screen'>
+    <main className={`bg-black min-h-screen ${montserrat.className}  px-10 py-2`}>
       <Navbar />
     </main>
   )
