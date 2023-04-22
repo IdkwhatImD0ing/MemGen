1b220c1939481253285a917a291b084955fd0f72
Bill Zhang <billzhangsc@gmail.com>
2023-04-22 03:11:04 -0700

    Update initialization

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,19 @@
 var express = require('express')
 var router = express.Router()
+var serviceAccount = require('../firebase.json')
+const {Configuration, OpenAIApi} = require('openai')
+const axios = require('axios')
+
+let defaultApp = admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+})
+let defaultDatabase = admin.firestore(defaultApp)
+
+// Openai Setup
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+})
+const openai = new OpenAIApi(configuration)
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -7,19 +21,31 @@ router.get('/', function (req, res, next) {
 })
 
 /* POST route to handle JSON input */
-router.post('/query', function (req, res, next) {
+router.post('/query', async function (req, res, next) {
   const {userid, text} = req.body
 
   if (userid && text) {
     // Process the data as needed
-    res.status(200).json({message: 'Data received', userid, text})
+    const embedding = await fetchEmbedding(text)  
   } else {
-    res
-      .status(400)
-      .json({
-        message: 'Bad request. Please provide both userid and text fields.',
-      })
+    res.status(400).json({
+      message: 'Bad request. Please provide both userid and text fields.',
+    })
   }
 })
 
 module.exports = router
+
+async function fetchEmbedding(text) {
+  try {
+    const response = await openai.createEmbedding({
+      model: 'text-embedding-ada-002',
+      input: text,
+    })
+
+    return response.data
+  } catch (error) {
+    console.error('Error fetching embedding:', error)
+    throw error
+  }
+}
