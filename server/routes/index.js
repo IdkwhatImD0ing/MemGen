f1eb9192594e9e53984b4634d9d6d3fd798e1bd9
Bill Zhang <billzhangsc@gmail.com>
2023-04-22 02:11:34 -0700

    openai and firebase

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,25 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', function (req, res, next) {
+  res.render('index', {title: 'Express'})
+})
 
-module.exports = router;
+/* POST route to handle JSON input */
+router.post('/query', function (req, res, next) {
+  const {userid, text} = req.body
+
+  if (userid && text) {
+    // Process the data as needed
+    res.status(200).json({message: 'Data received', userid, text})
+  } else {
+    res
+      .status(400)
+      .json({
+        message: 'Bad request. Please provide both userid and text fields.',
+      })
+  }
+})
+
+module.exports = router
