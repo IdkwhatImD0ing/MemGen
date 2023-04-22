88c4b1e4a7b0852268244d8c9a954bdd3cba02e2
Bill Zhang <billzhangsc@gmail.com>
2023-04-22 16:01:34 -0700

    Docker!

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,73 +1,62 @@
-var express = require("express");
-var router = express.Router();
-var serviceAccount = require("../firebase.json");
-const { Configuration, OpenAIApi } = require("openai");
-const multer = require("multer");
-const pdfParse = require("pdf-parse");
-const upload = multer({ storage: multer.memoryStorage() });
-const axios = require("axios");
-const {
-  MilvusClient,
-  DataType,
-  MetricType,
-} = require("@zilliz/milvus2-sdk-node");
-const config = require("../config.js");
-const { uri, user, password, secure } = config;
-const milvusClient = new MilvusClient(uri, secure, user, password, secure);
-
-// // Images
-// const {fromBuffer} = require('pdf2pic')
-// const {PDFDocument} = require('pdf-lib')
-
-// const Busboy = require('busboy')
-// const {ImageAnnotatorClient} = require('@google-cloud/vision')
-// const client = new ImageAnnotatorClient()
+var express = require('express')
+var router = express.Router()
+var serviceAccount = require('../firebase.json')
+const {Configuration, OpenAIApi} = require('openai')
+const multer = require('multer')
+const pdfParse = require('pdf-parse')
+const upload = multer({storage: multer.memoryStorage()})
+const axios = require('axios')
+const {MilvusClient, DataType, MetricType} = require('@zilliz/milvus2-sdk-node')
+const config = require('../config.js')
+const {uri, user, password, secure} = config
+const path = require('path')
+const milvusClient = new MilvusClient(uri, secure, user, password, secure)
 
 // Uuid
-const { v4: uuidv4 } = require("uuid");
+const {v4: uuidv4} = require('uuid')
 
 // Firebase Setup
-const admin = require("firebase-admin");
+const admin = require('firebase-admin')
 let defaultApp = admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-});
-let defaultDatabase = admin.firestore(defaultApp);
-let storage = admin.storage(defaultApp);
-const bucket = storage.bucket("images");
-const tempDir = "temp-images/";
+})
+let defaultDatabase = admin.firestore(defaultApp)
+let storage = admin.storage(defaultApp)
+const bucket = storage.bucket('images')
+const tempDir = 'temp-images/'
 
 // Openai Setup
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
+})
+const openai = new OpenAIApi(configuration)
 
 // Cohere Setup
-const cohere = require("cohere-ai");
-cohere.init(process.env.COHERE_API_KEY);
+const cohere = require('cohere-ai')
+cohere.init(process.env.COHERE_API_KEY)
 
 /* GET home page. */
-router.get("/", function (req, res) {
-  res.render("index", { title: "Express" });
-});
+router.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'file.html'))
+})
 
-router.post("/add", async function (req, res) {
-  const { userid, text } = req.body;
-  const embedding = await fetchEmbedding(text);
-  const uuid = uuidv4();
+router.post('/add', async function (req, res) {
+  const {userid, text} = req.body
+  const embedding = await fetchEmbedding(text)
+  const uuid = uuidv4()
 
   // Add the text and embedding to Firebase
-  const userCollection = defaultDatabase.collection(userid);
-  const document = userCollection.doc(uuid);
+  const userCollection = defaultDatabase.collection(userid)
+  const document = userCollection.doc(uuid)
 
   try {
     await document.set({
       text: text,
       embedding: embedding,
-    });
+    })
 
     const data = {
-      collection_name: "Resume",
+      collection_name: 'Resume',
       fields_data: [
         {
           uuid: uuid,
@@ -75,80 +64,80 @@ router.post("/add", async function (req, res) {
           userid: userid,
         },
       ],
-    };
+    }
 
-    const ret = await milvusClient.insert(data);
+    const ret = await milvusClient.insert(data)
 
-    res.status(200).json({ message: "success", ret });
+    res.status(200).json({message: 'success', ret})
   } catch (error) {
     // Rollback: delete the document in Firebase if it was added
-    const docSnapshot = await document.get();
+    const docSnapshot = await document.get()
     if (docSnapshot.exists) {
-      await document.delete();
+      await document.delete()
     }
 
     res.status(500).json({
-      message: "An error occurred while processing the transaction.",
+      message: 'An error occurred while processing the transaction.',
       error: error.message,
-    });
+    })
   }
-});
+})
 
 /* POST route to handle JSON input */
-router.post("/query", async function (req, res) {
-  const { userid, text } = req.body;
+router.post('/query', async function (req, res) {
+  const {userid, text} = req.body
 
   if (userid && text) {
     // Reload collection
     await milvusClient.loadCollection({
-      collection_name: "Resume",
-    });
+      collection_name: 'Resume',
+    })
 
     // Process the data as needed
-    const embedding = await fetchEmbedding(text);
+    const embedding = await fetchEmbedding(text)
     const searchParams = {
-      anns_field: "vector",
+      anns_field: 'vector',
       topk: 3,
       metric_type: MetricType.L2,
-      params: JSON.stringify({ nprobe: 1024 }),
-    };
+      params: JSON.stringify({nprobe: 1024}),
+    }
     const searchReq = {
-      collection_name: "Resume",
+      collection_name: 'Resume',
       vectors: [embedding],
       search_params: searchParams,
       vector_type: DataType.FloatVector,
       expr: `userid == "${userid}"`,
-      output_fields: ["uuid"],
-    };
+      output_fields: ['uuid'],
+    }
 
-    const searchResults = await milvusClient.search(searchReq);
+    const searchResults = await milvusClient.search(searchReq)
     // Process the search results
     const messagePromises = searchResults.results.map(async (result) => {
       // Find the corresponding document in firebase
-      const id = result.id;
-      const docRef = defaultDatabase.collection(userid).doc(id);
-      const doc = await docRef.get();
+      const id = result.id
+      const docRef = defaultDatabase.collection(userid).doc(id)
+      const doc = await docRef.get()
 
-      const data = doc.data();
-      const text = data.text;
-      console.log(text);
-      return text;
-    });
+      const data = doc.data()
+      const text = data.text
+      console.log(text)
+      return text
+    })
 
     // Wait for all promises to resolve
-    const messageArray = await Promise.all(messagePromises);
+    const messageArray = await Promise.all(messagePromises)
 
     // Send the response
-    res.status(200).json({ message: "success", data: messageArray });
+    res.status(200).json({message: 'success', data: messageArray})
   } else {
     res.status(400).json({
-      message: "Bad request. Please provide both userid and text fields.",
-    });
+      message: 'Bad request. Please provide both userid and text fields.',
+    })
   }
-});
+})
 
-router.post("/generate", async function (req, res) {
-  const { description, text } = req.body;
+router.post('/generate', async function (req, res) {
+  const {description, text} = req.body
 
   if (text && description) {
     try {
@@ -156,193 +145,60 @@ router.post("/generate", async function (req, res) {
 Previous experiences: ${text}
 
 Write a cover letter that matches the job description and utilizes the previous experiences provided:
-`;
+`
       const response = await cohere.generate({
-        model: "command-xlarge-nightly",
+        model: 'command-xlarge-nightly',
         prompt: prompt,
         max_tokens: 2000,
         temperature: 0.8,
         k: 0,
         stop_sequences: [],
-        return_likelihoods: "NONE",
-      });
+        return_likelihoods: 'NONE',
+      })
 
-      res.status(200).json({ message: "success", data: response });
+      res.status(200).json({message: 'success', data: response})
     } catch (error) {
       res.status(500).json({
-        message: "An error occurred while processing your request.",
+        message: 'An error occurred while processing your request.',
         error: error.message,
-      });
+      })
     }
   } else {
     res.status(400).json({
       message:
         'Bad request. Please provide a JSON string in the "jsonPrompt" field.',
-    });
+    })
   }
-});
+})
 
-router.post("/upload", upload.single("pdf"), async (req, res) => {
+router.post('/upload', upload.single('pdf'), async (req, res) => {
   //PDF to text
   if (!req.file) {
-    console.error('No file received');
-    return res.status(400).send('No file received');
+    console.error('No file received')
+    return res.status(400).send('No file received')
   }
   try {
-    const pdfBuffer = req.file.buffer;
-    const pdfData = await pdfParse(pdfBuffer);
-    res.send(pdfData.text);
+    const pdfBuffer = req.file.buffer
+    const pdfData = await pdfParse(pdfBuffer)
+    res.send(pdfData.text)
   } catch (error) {
-    console.error('Error processing PDF:', error);
-    res.status(500).send('Error processing PDF');
+    console.error('Error processing PDF:', error)
+    res.status(500).send('Error processing PDF')
   }
-});
+})
 
-// router.post('/upload', (req, res) => {
-//   req.headers['content-type'] =
-//     req.headers['content-type'] || req.headers['Content-Type']
-//   const busboy = Busboy({headers: req.headers, autoFields: true})
-//   console.log('Content-Type:', req.headers['content-type'])
-
-//   busboy.on('file', async (name, file, info) => {
-//     const {filename, encoding, mimeType} = info
-//     console.log(
-//       `File [${name}]: filename: %j, encoding: %j, mimeType: %j`,
-//       filename,
-//       encoding,
-//       mimeType,
-//     )
-//     if (mimeType === 'application/pdf') {
-//       try {
-//         const pdfBuffer = await streamToBuffer(file)
-
-//         const pdfDoc = await pdfjs.getDocument({data: pdfBuffer}).promise
-//         const pdfDocLib = await PDFDocument.load(pdfBuffer)
-//         const totalPages = pdfDocLib.getPageCount()
-//         console.log(`Total pages: ${totalPages}`)
-
-//         const uploadedImages = await Promise.all(
-//           Array.from({length: totalPages}, async (_, index) => {
-//             const pageNumber = index + 1
-
-//             const page = await pdfDoc.getPage(pageNumber)
-//             const imageDataUrl = await pdfPageToImage(page)
-
-//             const imageBuffer = Buffer.from(
-//               imageDataUrl.split(',')[1],
-//               'base64',
-//             )
-
-//             const imageFileName = `pdf-images/${Date.now()}_${pageNumber}.png`
-//             const imageFile = bucket.file(imageFileName)
-//             await imageFile.save(imageBuffer, {
-//               metadata: {contentType: 'image/png'},
-//             })
-
-//             return `gs://${bucket.name}/${imageFileName}`
-//           }),
-//         )
-
-//         // Prepare the requests for Google Cloud Vision API
-//         const inputConfig = {
-//           mimeType: 'image/png',
-//         }
-//         const requests = uploadedImages.map((gcsImageUri) => ({
-//           inputConfig,
-//           gcsImage: {gcsImageUri},
-//           features: [{type: 'DOCUMENT_TEXT_DETECTION'}],
-//         }))
-
-//         console.log('Sending requests to Google Cloud Vision API...')
-
-//         // Create asyncBatchAnnotateFiles request
-//         const asyncRequest = {
-//           requests,
-//           outputConfig: {
-//             gcsDestination: {
-//               uri: `gs://${bucket.name}/text-output/`,
-//             },
-//           },
-//         }
-
-//         // Call files:asyncBatchAnnotate function
-//         const [operation] = await client.asyncBatchAnnotateFiles(asyncRequest)
-//         const [filesResponse] = await operation.promise()
-
-//         console.log('Processing the response...')
-
-//         // Get the JSON output file
-//         const outputUri =
-//           filesResponse.responses[0].outputConfig.gcsDestination.uri
-//         const jsonFileName = outputUri.replace('gs://' + bucket.name + '/', '')
-//         const [jsonFile] = await bucket.file(jsonFileName).download()
-
-//         console.log('Extracting text from the response...')
-
-//         // Parse the JSON file and extract text
-//         const jsonContent = JSON.parse(jsonFile.toString())
-//         const fullText = jsonContent.responses
-//           .map((response) => response.fullTextAnnotation.text)
-//           .join('\n')
-
-//         console.log('Sending the response...')
-//         console.log(fullText)
-
-//         // Send the extracted text as the response
-//         res.send(fullText)
-//       } catch (error) {
-//         console.error(error)
-//         res.status(500).send('An error occurred while processing the PDF.')
-//       }
-//     } else {
-//       res.status(400).send('Invalid file type. Please upload a PDF.')
-//     }
-//   })
-
-//   busboy.on('error', (error) => {
-//     console.error('Busboy error:', error)
-//   })
-
-//   busboy.on('finish', () => {
-//     console.log('Busboy finished parsing form')
-//   })
-
-//   req.pipe(busboy)
-// })
-
-module.exports = router;
+module.exports = router
 
 async function fetchEmbedding(text) {
   try {
     const response = await openai.createEmbedding({
-      model: "text-embedding-ada-002",
+      model: 'text-embedding-ada-002',
       input: text,
-    });
+    })
 
-    return response.data.data[0].embedding;
+    return response.data.data[0].embedding
   } catch (error) {
-    console.error("Error fetching embedding:", error);
-    throw error;
+    console.error('Error fetching embedding:', error)
+    throw error
   }
-}
-
-function streamToBuffer(stream) {
-  return new Promise((resolve, reject) => {
-    const chunks = [];
-    stream.on("data", (chunk) => chunks.push(chunk));
-    stream.on("error", reject);
-    stream.on("end", () => resolve(Buffer.concat(chunks)));
-  });
-}
-
-async function pdfPageToImage(page) {
-  const viewport = page.getViewport({ scale: 1 });
-  const canvasFactory = new pdfjs.DOMCanvasFactory();
-  const renderingContext = {
-    canvasContext: canvasFactory.create(viewport.width, viewport.height),
-    viewport: viewport,
-    canvasFactory: canvasFactory,
-  };
-  await page.render(renderingContext).promise;
-  return renderingContext.canvasContext.canvas.toDataURL();
 }
