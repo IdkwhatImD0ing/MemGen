3299647438b87f64c334f4bcee711ed0500d0cd9
Trique Nguyen <ngngoctridung123@gmail.com>
2023-04-22 01:59:19 -0700

    refining landingpage

--- client/pages/index.js
+++ client/pages/index.js
@@ -4,10 +4,11 @@ import Navbar from "./components/Navbar";
 import LandingPage from "./components/LandingPage";
 
 const inter = Inter({ subsets: ['latin'] })
+const montserrat = Montserrat({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className='bg-black min-h-screen'>
+    <main className={`bg-black min-h-screen px-10 py-2 ${montserrat.className}`}>
       <Navbar />
       <LandingPage />
     </main>
