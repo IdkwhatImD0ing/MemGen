4225036aeeafdfc9cb3e23cd6ae9bf24436e9bc1
skumar <53668449+sidgithub780@users.noreply.github.com>
2023-04-22 10:02:56 -0700

    added inputdoc to nav and pages

--- client/pages/components/Navbar2.jsx
+++ client/pages/components/Navbar2.jsx
@@ -6,6 +6,7 @@ export default function Navbar() {
   return (
     <nav className="flex text-white items-center justify-centers mr-10">
       <Link href="/" className='mr-auto'><Image src="/memgen.svg" alt='logo' width={200} height={200} /></Link>
+      <Link href="/inputdocuments" className="px-4 py-2 hover:bg-slate-700 rounded-md text-lg font-semibol mx-2">Input Documents</Link>
       <Link href="/api/auth/logout" className="px-6 py-2 text-black bg-white hover:scale-105 active:scale-95 rounded-md font-semibold text-lg">Logout</Link>
 
     </nav>
