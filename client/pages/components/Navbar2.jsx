51d8c4f69dc32283f25677e3177dfbc640dd2f0a
Siddhanth Kumar <53668449+sidgithub780@users.noreply.github.com>
2023-04-22 08:40:59 -0700

    Revert "frontend updates"

--- client/pages/components/Navbar2.jsx
+++ client/pages/components/Navbar2.jsx
@@ -6,8 +6,6 @@ export default function Navbar() {
   return (
     <nav className="flex text-white items-center justify-centers mr-10">
       <Link href="/" className='mr-auto'><Image src="/memgen.svg" alt='logo' width={200} height={200} /></Link>
-      <Link href="/inputdocument" className="px-4 py-2 hover:bg-slate-700 rounded-md font-semibold text-lg mr-2">Input Document</Link>
-
       <Link href="/api/auth/logout" className="px-6 py-2 text-black bg-white hover:scale-105 active:scale-95 rounded-md font-semibold text-lg">Logout</Link>
 
     </nav>
