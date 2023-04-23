2ffc62cc103d3074c705f0d09ff56964abcdd419
Bill Zhang <billzhangsc@gmail.com>
2023-04-22 22:47:01 -0700

    Modify top bar

--- client/pages/components/Navbar.jsx
+++ client/pages/components/Navbar.jsx
@@ -41,15 +41,9 @@ export default function Navbar() {
           </Link>
           <Link
             href="/api/auth/login"
-            className="px-4 py-2 hover:bg-slate-700 rounded-md"
-          >
-            Login
-          </Link>
-          <Link
-            href="/homepage"
             className="px-4 py-2 text-black bg-white hover:scale-105 active:scale-95 rounded-md"
           >
-            Get Started
+            Login
           </Link>
         </div>
       )}
