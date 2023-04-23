abba64f2b393820e25946ae29ecbd3a373f30c1f
Bill Zhang <billzhangsc@gmail.com>
2023-04-22 20:36:51 -0700

    Analytics

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,12 +1,14 @@
 import '@/styles/globals.css'
+import {Analytics} from '@vercel/analytics/react'
 // pages/_app.js
-import React from 'react';
-import { UserProvider } from '@auth0/nextjs-auth0/client';
+import React from 'react'
+import {UserProvider} from '@auth0/nextjs-auth0/client'
 
-export default function App({ Component, pageProps }) {
+export default function App({Component, pageProps}) {
   return (
     <UserProvider>
       <Component {...pageProps} />
+      <Analytics />
     </UserProvider>
-  );
+  )
 }
